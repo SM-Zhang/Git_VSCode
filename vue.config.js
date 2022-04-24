a0e1ac933e68ea6755c9666a93c9615fae04587b
SM-Zhang <1097646307@qq.com>
2022-04-24 10:32:19 +0800

    配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
+// defineConfig 帮手函数，以获得更好的类型提示
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
 })
